668095f7b17217f98e4827946af4f5d31ba4faa7
Vidur Agarwal <geurecruitviduragarwal@gmail.com>
2024-01-15 00:16:17 +0530

    To Do App

---
+++ src/styles/theme.tsx
@@ -0,0 +1,40 @@
+import { createTheme } from "@mui/material";
+
+export const ColorPalette = {
+  fontDark: "#101727",
+  fontLight: "#F5F5F5",
+  purple: "#b624ff",
+  red: "#ff3131",
+};
+
+export const MuiTheme = createTheme({
+  components: {
+    MuiTooltip: {
+      defaultProps: {
+        disableInteractive: true,
+      },
+    },
+    MuiSelect: {
+      defaultProps: {
+        style: {
+          borderRadius: "18px",
+        },
+      },
+    },
+  },
+  typography: {
+    fontFamily: '"Poppins", sans-serif',
+  },
+
+  palette: {
+    primary: {
+      main: ColorPalette.purple,
+    },
+    secondary: {
+      main: "#bababa",
+    },
+    error: {
+      main: ColorPalette.red,
+    },
+  },
+});
